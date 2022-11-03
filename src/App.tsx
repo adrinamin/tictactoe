4b64cb7d391f58511a965f0151af68c174389610
Adrin Amin Salehi <adrin.amin@gmail.com>
2022-11-03 17:12:31 +0000

    Working implementation

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,7 @@ class Board extends Component<BoardProps, {}> {
 
 interface GameState {
   history: { squares: string[] }[],
+  stepNumber: number;
   xIsNext: boolean;
 }
 
@@ -68,12 +69,13 @@ class Game extends Component<{}, GameState> {
       history: [{        
         squares: Array(9).fill(null),      
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   private handleClick(i : any) : void {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {      
@@ -82,14 +84,34 @@ class Game extends Component<{}, GameState> {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo(step: number) : void {    
+    this.setState({      
+      stepNumber: step,      
+      xIsNext: (step % 2) === 0,    
+    });  
+  }
+
   render() {
     const history = this.state.history;    
-    const current = history[history.length - 1];    
-    const winner = calculateWinner(current.squares);    
+    const current = history[this.state.stepNumber];  
+    const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {      
+      const desc = move ?        
+      'Go to move #' + move :        
+      'Go to game start';      
+      return (        
+        <li key={move}>          
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>        
+        </li>      
+      );    
+    });
+    
     let status;    
     if (winner) {      
       status = 'Winner: ' + winner;    
@@ -105,7 +127,7 @@ class Game extends Component<{}, GameState> {
         </div>
         <div className="game-info">
           <div>{status}</div>          
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
